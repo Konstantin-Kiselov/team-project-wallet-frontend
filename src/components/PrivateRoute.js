b0efc8a0d0a87af08f9e7d0f7978e8bb73d2bd66
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-13 23:57:04 +0100

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,12 +1,8 @@
 import { useSelector } from 'react-redux';
-import { Route, Redirect } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { authSelectors } from '../redux/auth';
 
-export default function PrivateRoute({ redirectTo, children, ...routePropps }) {
+export default function PrivateRoute({ children, redirectTo = '/' }) {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
-  return (
-    <Route {...routePropps}>
-      {isLoggedIn ? children : <Redirect to={redirectTo} />}
-    </Route>
-  );
+  return isLoggedIn ? children : <Navigate to={redirectTo} />;
 }
