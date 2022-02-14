696b9c9fc63945ae1f312ae730897d248314817e
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-14 22:37:15 +0100

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,27 +1,49 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 import s from './AuthNav.module.css';
 
+const setActiveClass = ({ isActive }) => (isActive ? 'active-link' : 'link');
+
 export default function AuthNav() {
   return (
-    <>
-      <div className={s.commonContainer}>
-        <div className={s.frame}></div>
-        <div className={s.rightSide}></div>
+    <div className={s.commonContainer}>
+      <div className={s.frame}></div>
+      <div className={s.leftSide}>
+        <img className={s.frameLog} src="/frame-log.png" alt="" />
+        <h1 className={s.titleLeftSide}>Finance App</h1>
+      </div>
+
+      <div className={s.rightSide}>
         <div className={s.form}>
-          <NavLink to="/login" className={s.link}>
-            <button type="button" className={s.buttonLogin}>
-              Вход
-            </button>
+          <h2 className={s.titleRightSide}>Wallet</h2>
+
+          <Outlet />
+
+          <NavLink
+            to="/login"
+            className={setActiveClass}
+            style={({ isActive }) => ({
+              marginBottom: isActive ? '20px' : '0px',
+            })}
+          >
+            Вход
           </NavLink>
-          <NavLink to="/register" className={s.link}>
-            <button type="button" className={s.buttonRegister}>
-              Регистрация
-            </button>
+
+          <NavLink
+            to="/register"
+            className={setActiveClass}
+            style={({ isActive }) => ({
+              marginTop: isActive ? '20px' : '0px',
+              marginBottom: '0px',
+            })}
+          >
+            Регистрация
           </NavLink>
         </div>
       </div>
-    </>
+
+      {/*<div className={s.form}></div>*/}
+    </div>
   );
 }
