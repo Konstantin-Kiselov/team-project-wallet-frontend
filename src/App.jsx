b25dd904fc7cab6f4e39c226deed58b437d83891
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-14 22:37:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import AppBar from './components/AppBar/AppBar';
 import Container from './components/Container/Container';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 //import PrivateRoute from '../components/PrivateRoute.js';
 import PublicRoute from './components/PublicRoute';
@@ -13,6 +13,7 @@ import AuthNav from './components/AuthNav';
 // const HomeView = lazy(() => import('../views/HomeView/HomeView'));
 const RegisterView = lazy(() => import('./views/RegisterView'));
 const LoginView = lazy(() => import('./views/LoginView'));
+const NotFoundView = lazy(() => import('./views/NotFoundView'));
 
 export default function App() {
   // const dispatch = useDispatch();
@@ -21,29 +22,29 @@ export default function App() {
     <Container>
       <AppBar />
 
-      <AuthNav />
       <Suspense fallback={<p>Загружаем...</p>}>
         <Routes>
-          <Route
-            path="/register"
-            element={
-              <PublicRoute restricted>
-                <RegisterView />
-              </PublicRoute>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <PublicRoute restricted redirectTo="/home">
-                <LoginView />
-              </PublicRoute>
-            }
-          />
+          <Route path="/" element={<AuthNav />}>
+            <Route index element={<LoginView />} />
+            <Route
+              path="register"
+              element={
+                <PublicRoute restricted>
+                  <RegisterView />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="login"
+              element={
+                <PublicRoute restricted redirectTo="/home">
+                  <LoginView />
+                </PublicRoute>
+              }
+            />
 
-          {/*<Route>
-              <NotFoundPage />
-            </Route>*/}
+            <Route path="*" element={<NotFoundView />} />
+          </Route>
         </Routes>
       </Suspense>
     </Container>
