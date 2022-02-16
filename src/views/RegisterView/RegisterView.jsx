647c516d56deae898241da631a28e9943313094f
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-16 05:23:37 +0100

    Update RegisterView.jsx

--- src/views/RegisterView/RegisterView.jsx
+++ src/views/RegisterView/RegisterView.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { authOperations } from '../../redux/auth';
 
-import TextField from '@mui/material/TextField';
+import { Box, TextField } from '@mui/material';
+import LocalPostOfficeIcon from '@mui/icons-material/LocalPostOffice';
+import LockIcon from '@mui/icons-material/Lock';
 
 import s from './RegisterView.module.css';
 
@@ -36,25 +38,98 @@ export default function RegisterView() {
   return (
     <div>
       <form onSubmit={handleSubmit} className={s.form} autoComplete="off">
-        <TextField
-          sx={{ mb: '40px' }}
-          id="standard-basic"
-          label="E-mail"
-          variant="standard"
-        />
-        <TextField
-          sx={{ mb: '40px' }}
-          id="standard-basic"
-          label="Пароль"
-          variant="standard"
-        />
-        <TextField
-          sx={{ mb: '40px' }}
-          id="standard-basic"
-          label="Подтвердите пароль"
-          variant="standard"
-        />
-        <TextField id="standard-basic" label="Ваше имя" variant="standard" />
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            mb: '40px',
+          }}
+        >
+          <TextField
+            className={s.TextField}
+            id="input-with-sx"
+            label="E-mail"
+            type="email"
+            variant="standard"
+            sx={{ left: '-30px' }}
+            fullWidth
+            required
+            InputProps={{
+              startAdornment: (
+                <LocalPostOfficeIcon
+                  sx={{ color: 'action.active', mr: 1, my: 0.5 }}
+                />
+              ),
+            }}
+            placeholder="E-mail"
+          />
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'flex-end', mb: '40px' }}>
+          <TextField
+            className={s.TextField}
+            type="password"
+            id="input-with-sx"
+            label="Пароль"
+            variant="standard"
+            sx={{ left: '-30px' }}
+            fullWidth
+            required
+            InputProps={{
+              startAdornment: (
+                <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              ),
+            }}
+            placeholder="Пароль"
+          />
+        </Box>
+
+        <Box sx={{ display: 'flex', alignItems: 'flex-end', mb: '40px' }}>
+          <TextField
+            className={s.TextField}
+            type="password"
+            id="input-with-sx"
+            label="Подтвердите пароль"
+            variant="standard"
+            sx={{ left: '-30px' }}
+            fullWidth
+            required
+            InputProps={{
+              startAdornment: (
+                <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+              ),
+            }}
+            placeholder="Подтвердите пароль"
+          />
+        </Box>
+
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'flex-end',
+            mb: '40px',
+          }}
+        >
+          <TextField
+            className={s.TextField}
+            id="input-with-sx"
+            label="Ваше имя"
+            type="text"
+            variant="standard"
+            sx={{ left: '-30px' }}
+            fullWidth
+            required
+            InputProps={{
+              startAdornment: (
+                <LocalPostOfficeIcon
+                  sx={{ color: 'action.active', mr: 1, my: 0.5 }}
+                />
+              ),
+            }}
+            placeholder="Ваше имя"
+          />
+        </Box>
+
         {/*<label style={styles.label}>
           Name
           <input type="text" name="name" value={name} onChange={handleChange} />
