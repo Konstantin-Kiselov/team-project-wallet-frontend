8983c8514a27c890df5fe2f7f3d3801ed7b0afb1
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-16 09:44:26 +0100

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
 import s from './AuthNav.module.css';
@@ -6,21 +7,31 @@ import s from './AuthNav.module.css';
 import walletIcon from '../../img/log&reg/wallet.svg';
 import loginFrame from '../../img/log&reg/login/frame-log.png';
 import registerFrame from '../../img/log&reg/registration/frame-reg.png';
+import violetEllipse from '../../img/log&reg/ellipse_violet.svg';
+import pinkEllipse from '../../img/log&reg/ellipse_pink.svg';
 
 const setActiveClass = ({ isActive }) => (isActive ? 'active-link' : 'link');
-const setActiveImg = ({ isActive }) =>
-  isActive ? 'loginFrame' : 'registerFrame';
 
 export default function AuthNav() {
+  const handleClick = e => {
+    e.preventDefault();
+  };
+
   return (
     <div className={s.commonContainer}>
       <div className={s.frame}></div>
       <div className={s.leftSide}>
-        <img alt="login frame" src={loginFrame} className={setActiveImg} />
+        <img alt="login frame" src={loginFrame} className={s.frameLog} />
+        <img
+          alt="violet ellipse"
+          src={violetEllipse}
+          className={s.violetEllipse}
+        />
         <h1 className={s.titleLeftSide}>Finance App</h1>
       </div>
 
       <div className={s.rightSide}>
+        <img alt="pink ellipse" src={pinkEllipse} className={s.pinkEllipse} />
         <div className={s.form}>
           <div className={s.titleRightSideContainer}>
             <img src={walletIcon} alt="wallet icon" />
@@ -30,8 +41,11 @@ export default function AuthNav() {
           <Outlet />
 
           <NavLink
+            onClick={''}
             to="/login"
             className={setActiveClass}
+            data-name={'login'}
+            id="login"
             style={({ isActive }) => ({
               marginBottom: isActive ? '20px' : '0px',
             })}
@@ -39,8 +53,11 @@ export default function AuthNav() {
             Вход
           </NavLink>
           <NavLink
+            onClick={''}
             to="/register"
             className={setActiveClass}
+            data-name={'register'}
+            id="register"
             style={({ isActive }) => ({
               marginTop: isActive ? '20px' : '0px',
               marginBottom: '0px',
