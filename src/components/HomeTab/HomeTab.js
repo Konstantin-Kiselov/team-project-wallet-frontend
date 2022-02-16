211e3fe96ec351fcd51604a087db438e17c0d69a
OlyaArta <77747461+OlyaArta@users.noreply.github.com>
2022-02-16 20:39:45 +0200

    add styles

--- src/components/HomeTab/HomeTab.js
+++ src/components/HomeTab/HomeTab.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import transactions from './transactions.json';
 import s from './HomeTab.module.css';
+import classNames from 'classnames';
 
 export default function HomeTab() {
   const [status, setStatus] = useState('idle');
@@ -19,17 +20,25 @@ export default function HomeTab() {
   //       });
   //   }, []);
 
+  // const data = transactions.map()
+
   return (
     <div className={s.container}>
       <table className={s.table}>
         <thead className={s.titles}>
           <tr className={s.row}>
             <th className={s.titles_head}>Дата</th>
-            <th className={s.titles_head}>Тип</th>
-            <th className={s.titles_head}>Категория</th>
+            <th className={classNames(s.titles_head, s.body_item_type)}>Тип</th>
+            <th className={classNames(s.titles_head, s.titles_head_category)}>
+              Категория
+            </th>
             <th className={s.titles_head}>Комментарий</th>
-            <th className={s.titles_head}>Сумма</th>
-            <th className={s.titles_head}>Баланс</th>
+            <th className={classNames(s.titles_head, s.body_item_type)}>
+              Сумма
+            </th>
+            <th className={classNames(s.titles_head, s.body_item_type)}>
+              Баланс
+            </th>
           </tr>
         </thead>
         <tbody className={s.body}>
@@ -37,80 +46,32 @@ export default function HomeTab() {
             <td aria-label="Дата" className={s.body_item}>
               12.02.1557
             </td>
-            <td aria-label="Тип" className={s.body_item}>
+            <td
+              aria-label="Тип"
+              className={classNames(s.body_item, s.body_item_type)}
+            >
               -
             </td>
-            <td aria-label="Категория" className={s.body_item}>
+            <td
+              aria-label="Категория"
+              className={classNames(s.body_item, s.body_item_category)}
+            >
               Машина
             </td>
             <td aria-label="Комментарий" className={s.body_item}>
               купили фары
             </td>
-            <td aria-label="Сумма" className={s.body_item}>
-              3600
+            <td
+              aria-label="Сумма"
+              className={classNames(s.body_item, s.body_item_sum)}
+            >
+              3 600.00
             </td>
-            <td aria-label="Баланс" className={s.body_item}>
-              12900
-            </td>
-          </tr>
-          <tr className={s.row}>
-            <td aria-label="Дата" className={s.body_item}>
-              12.02.1557
-            </td>
-            <td aria-label="Тип" className={s.body_item}>
-              -
-            </td>
-            <td aria-label="Категория" className={s.body_item}>
-              Машина
-            </td>
-            <td aria-label="Комментарий" className={s.body_item}>
-              купили фары
-            </td>
-            <td aria-label="Сумма" className={s.body_item}>
-              3600
-            </td>
-            <td aria-label="Баланс" className={s.body_item}>
-              12900
-            </td>
-          </tr>
-          <tr className={s.row}>
-            <td aria-label="Дата" className={s.body_item}>
-              12.02.1557
-            </td>
-            <td aria-label="Тип" className={s.body_item}>
-              -
-            </td>
-            <td aria-label="Категория" className={s.body_item}>
-              Машина
-            </td>
-            <td aria-label="Комментарий" className={s.body_item}>
-              купили фары
-            </td>
-            <td aria-label="Сумма" className={s.body_item}>
-              3600
-            </td>
-            <td aria-label="Баланс" className={s.body_item}>
-              12900
-            </td>
-          </tr>
-          <tr className={s.row}>
-            <td aria-label="Дата" className={s.body_item}>
-              12.02.1557
-            </td>
-            <td aria-label="Тип" className={s.body_item}>
-              -
-            </td>
-            <td aria-label="Категория" className={s.body_item}>
-              Машина
-            </td>
-            <td aria-label="Комментарий" className={s.body_item}>
-              купили фары
-            </td>
-            <td aria-label="Сумма" className={s.body_item}>
-              3600
-            </td>
-            <td aria-label="Баланс" className={s.body_item}>
-              12900
+            <td
+              aria-label="Баланс"
+              className={classNames(s.body_item, s.body_item_balance)}
+            >
+              12 900.00
             </td>
           </tr>
         </tbody>
