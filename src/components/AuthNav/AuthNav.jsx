bed8089ff7ca7ea7abc6eec6165db1df8b875145
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-17 11:52:47 +0100

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -18,56 +18,56 @@ export default function AuthNav() {
   };
 
   return (
-    <div className={s.commonContainer}>
-      <div className={s.leftSide}>
-        <img alt="login frame" src={loginFrame} className={s.frameLog} />
-        <img
-          alt="violet ellipse"
-          src={violetEllipse}
-          className={s.violetEllipse}
-        />
-        <h1 className={s.titleLeftSide}>Finance App</h1>
-      </div>
+    <>
+      {/*<div className={s.commonContainer}>*/}
+      {/*<div className={s.leftSide}>
+          <img alt="login frame" src={loginFrame} className={s.frameLog} />
+          <img
+            alt="violet ellipse"
+            src={violetEllipse}
+            className={s.violetEllipse}
+          />
+          <h1 className={s.titleLeftSide}>Finance App</h1>
+        </div>*/}
 
-      <div className={s.rightSide}>
-        <img alt="pink ellipse" src={pinkEllipse} className={s.pinkEllipse} />
-        <div className={s.form}>
-          <div className={s.titleRightSideContainer}>
-            <img src={walletIcon} alt="wallet icon" />
-            <h2 className={s.titleRightSide}>Wallet</h2>
-          </div>
+      {/*<div className={s.rightSide}>
+          <img alt="pink ellipse" src={pinkEllipse} className={s.pinkEllipse} />
+          <div className={s.form}>
+            <div className={s.titleRightSideContainer}>
+              <img src={walletIcon} alt="wallet icon" />
+              <h2 className={s.titleRightSide}>Wallet</h2>
+            </div>*/}
 
-          <Outlet />
+      <Outlet />
 
-          <NavLink
-            onClick={''}
-            to="/login"
-            className={setActiveClass}
-            data-name={'login'}
-            id="login"
-            style={({ isActive }) => ({
-              marginBottom: isActive ? '20px' : '0px',
-            })}
-          >
-            Вход
-          </NavLink>
-          <NavLink
-            onClick={''}
-            to="/register"
-            className={setActiveClass}
-            data-name={'register'}
-            id="register"
-            style={({ isActive }) => ({
-              marginTop: isActive ? '20px' : '0px',
-              marginBottom: '0px',
-            })}
-          >
-            Регистрация
-          </NavLink>
-        </div>
-      </div>
-
-      {/*<div className={s.form}></div>*/}
-    </div>
+      {/*<NavLink
+        onClick={''}
+        to="/login"
+        className={setActiveClass}
+        data-name={'login'}
+        id="login"
+        style={({ isActive }) => ({
+          marginBottom: isActive ? '20px' : '0px',
+        })}
+      >
+        Вход
+      </NavLink>
+      <NavLink
+        onClick={''}
+        to="/register"
+        className={setActiveClass}
+        data-name={'register'}
+        id="register"
+        style={({ isActive }) => ({
+          marginTop: isActive ? '20px' : '0px',
+          marginBottom: '0px',
+        })}
+      >
+        Регистрация
+      </NavLink>*/}
+      {/*</div>
+        </div>*/}
+      {/*</div>*/}
+    </>
   );
 }
