d8786438b7a645d29cdf3c467aab20e7ea818706
OlyaArta <77747461+OlyaArta@users.noreply.github.com>
2022-02-18 15:32:56 +0200

    add desktop, tablet and mobile version of hometab

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import AppBar from '../../components/AppBar/AppBar';
-import Currency from '../../components/Currency/Currency'
+import Currency from '../../components/Currency/Currency';
+import HomeTab from '../../components/HomeTab/HomeTab';
 
 export default function HomeView() {
   return (
     <>
       <AppBar />
       <Currency />
-    </>);
+      <HomeTab />
+    </>
+  );
 }
