d0a126bd3b4c5706c22bec442d94569c4aff5ad5
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-20 11:23:29 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useEffect, lazy, Suspense } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { animated, useTransition } from 'react-spring';
+
 import Container from './components/Container/Container';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 import PrivateRoute from './components/PrivateRoute.js';
 import PublicRoute from './components/PublicRoute';
@@ -18,40 +20,56 @@ const NotFoundView = lazy(() => import('./views/NotFoundView'));
 export default function App() {
   // const dispatch = useDispatch();
 
+  // add ANIMATION
+  const location = useLocation();
+  console.log(location);
+
+  const transitions = useTransition(location, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
   return (
-    <SectionAuthNav>
+    <SectionAuthNav style={{ position: 'relative' }}>
       {/* <Container> */}
       <Suspense fallback={<p>Загружаем...</p>}>
-        <Routes>
-          <Route path="/" element={<AuthNav />}>
-            <Route index element={<LoginView />} />
-            <Route
-              path="register"
-              element={
-                <PublicRoute restricted>
-                  <RegisterView />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="login"
-              element={
-                <PublicRoute restricted redirectTo="/home">
-                  <LoginView />
-                </PublicRoute>
-              }
-            />
-          </Route>
-          <Route
-            path="/home/*"
-            element={
-              <PrivateRoute restricted redirectTo="/login">
-                <HomeView />
-              </PrivateRoute>
-            }
-          />
-          <Route path="*" element={<NotFoundView />} />
-        </Routes>
+        {transitions((props, item) => (
+          <animated.div style={props}>
+            <div style={{ position: 'absolute', width: '100%' }}>
+              <Routes location={item}>
+                <Route path="/" element={<AuthNav />}>
+                  <Route index element={<LoginView />} />
+                  <Route
+                    path="register"
+                    element={
+                      <PublicRoute restricted>
+                        <RegisterView />
+                      </PublicRoute>
+                    }
+                  />
+                  <Route
+                    path="login"
+                    element={
+                      <PublicRoute restricted redirectTo="/home">
+                        <LoginView />
+                      </PublicRoute>
+                    }
+                  />
+                </Route>
+                <Route
+                  path="/home/*"
+                  element={
+                    <PrivateRoute restricted redirectTo="/login">
+                      <HomeView />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="*" element={<NotFoundView />} />
+              </Routes>
+            </div>
+          </animated.div>
+        ))}
       </Suspense>
       {/* </Container> */}
     </SectionAuthNav>
