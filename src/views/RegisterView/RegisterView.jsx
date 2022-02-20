ade8dcfb973bfb09ba9155ce7f2ec8ea52ce3992
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-20 09:27:33 +0100

    Update RegisterView.jsx

--- src/views/RegisterView/RegisterView.jsx
+++ src/views/RegisterView/RegisterView.jsx
@@ -16,6 +16,7 @@ import violetEllipse from '../../img/log&reg/ellipse_violet.svg';
 import pinkEllipse from '../../img/log&reg/ellipse_pink.svg';
 
 import PasswordStrengthMeter from '../../components/PasswordStrengthMeter/PasswordStrengthMeter';
+import Container from '../../components/Container/Container';
 
 const setActiveClass = ({ isActive }) => (isActive ? 'active-link' : 'link');
 
@@ -47,209 +48,226 @@ export default function RegisterView() {
   };
 
   return (
-    <div className={s.commonContainer}>
-      <div className={s.leftSide}>
-        <img alt="register frame" src={registerFrame} className={s.frameReg} />
-        <img
-          alt="violet ellipse"
-          src={violetEllipse}
-          className={s.violetEllipse}
-        />
-        <h1 className={s.titleLeftSide}>Finance App</h1>
-      </div>
+    <Container>
+      <div className={s.commonContainer}>
+        <div className={s.leftSide}>
+          <img
+            alt="register frame"
+            src={registerFrame}
+            className={s.frameReg}
+          />
+          <img
+            alt="violet ellipse"
+            src={violetEllipse}
+            className={s.violetEllipse}
+          />
+          <h1 className={s.titleLeftSide}>Finance App</h1>
+        </div>
 
-      <div className={s.rightSide}>
-        <img alt="pink ellipse" src={pinkEllipse} className={s.pinkEllipse} />
-        <div className={s.formContainer}>
-          <div className={s.titleRightSideContainer}>
-            <img src={walletIcon} alt="wallet icon" />
-            <h2 className={s.titleRightSide}>Wallet</h2>
-          </div>
+        <div className={s.rightSide}>
+          <img alt="pink ellipse" src={pinkEllipse} className={s.pinkEllipse} />
+          <div className={s.formContainer}>
+            <div className={s.titleRightSideContainer}>
+              <img src={walletIcon} alt="wallet icon" />
+              <h2 className={s.titleRightSide}>Wallet</h2>
+            </div>
 
-          <Formik
-            initialValues={{
-              email: '',
-              password: '',
-              confirmPassword: '',
-              name: '',
-            }}
-            validateOnBlur
-            onSubmit={handleSubmit}
-            validationSchema={registerSchema}
-            className={s.form}
-            autoComplete="off"
-          >
-            {({
-              values,
-              errors,
-              touched,
-              handleChange,
-              handleBlur,
-              isValid,
-              handleSubmit,
-              dirty,
-            }) => (
-              <>
-                {touched.email && errors.email && (
-                  <p className={s.error}>{errors.email}</p>
-                )}
-                <Box
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'flex-end',
-                    mb: '40px',
-                  }}
-                >
-                  <TextField
-                    className={s.TextField}
-                    id="input-with-sx"
-                    label="E-mail"
-                    type="email"
-                    variant="standard"
-                    fullWidth
-                    required
-                    name="email"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.email}
-                    InputProps={{
-                      startAdornment: (
-                        <LocalPostOfficeIcon
-                          sx={{ color: 'action.active', mr: 1, my: 0.5 }}
-                        />
-                      ),
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+                confirmPassword: '',
+                name: '',
+              }}
+              validateOnBlur
+              onSubmit={handleSubmit}
+              validationSchema={registerSchema}
+              className={s.form}
+              autoComplete="off"
+            >
+              {({
+                values,
+                errors,
+                touched,
+                handleChange,
+                handleBlur,
+                isValid,
+                handleSubmit,
+                dirty,
+              }) => (
+                <>
+                  {touched.email && errors.email && (
+                    <p className={s.error}>{errors.email}</p>
+                  )}
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'flex-end',
+                      mb: '40px',
+                      width: '100%',
                     }}
-                    placeholder="E-mail"
-                  />
-                </Box>
-                {touched.password && errors.password && (
-                  <p className={s.error}>{errors.password}</p>
-                )}
-                <Box
-                  sx={{ display: 'flex', alignItems: 'flex-end', mb: '40px' }}
-                >
-                  <TextField
-                    className={s.TextField}
-                    type="password"
-                    id="input-with-sx"
-                    label="Пароль"
-                    variant="standard"
-                    fullWidth
-                    required
-                    name="password"
-                    onChange={e => {
-                      setPassword(e.target.value);
-                      handleChange(e);
+                  >
+                    <TextField
+                      className={s.TextField}
+                      id="input-with-sx"
+                      label="E-mail"
+                      type="email"
+                      variant="standard"
+                      fullWidth
+                      required
+                      name="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                      InputProps={{
+                        startAdornment: (
+                          <LocalPostOfficeIcon
+                            sx={{ color: 'action.active', mr: 1, my: 0.5 }}
+                          />
+                        ),
+                      }}
+                      placeholder="E-mail"
+                    />
+                  </Box>
+                  {touched.password && errors.password && (
+                    <p className={s.error}>{errors.password}</p>
+                  )}
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'flex-end',
+                      mb: '',
+                      width: '100%',
                     }}
-                    onBlur={handleBlur}
-                    value={values.password}
-                    InputProps={{
-                      startAdornment: (
-                        <LockIcon
-                          sx={{ color: 'action.active', mr: 1, my: 0.5 }}
-                        />
-                      ),
+                  >
+                    <TextField
+                      className={s.TextField}
+                      type="password"
+                      id="input-with-sx"
+                      label="Пароль"
+                      variant="standard"
+                      fullWidth
+                      required
+                      name="password"
+                      onChange={e => {
+                        setPassword(e.target.value);
+                        handleChange(e);
+                      }}
+                      onBlur={handleBlur}
+                      value={values.password}
+                      InputProps={{
+                        startAdornment: (
+                          <LockIcon
+                            sx={{ color: 'action.active', mr: 1, my: 0.5 }}
+                          />
+                        ),
+                      }}
+                      placeholder="Пароль"
+                    />
+                  </Box>
+
+                  <PasswordStrengthMeter password={password} />
+
+                  {touched.confirmPassword && errors.confirmPassword && (
+                    <p className={s.error}>{errors.confirmPassword}</p>
+                  )}
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'flex-end',
+                      mb: '40px',
+                      width: '100%',
                     }}
-                    placeholder="Пароль"
-                  />
-                </Box>
-
-                <PasswordStrengthMeter password={password} />
-
-                {touched.confirmPassword && errors.confirmPassword && (
-                  <p className={s.error}>{errors.confirmPassword}</p>
-                )}
-                <Box
-                  sx={{ display: 'flex', alignItems: 'flex-end', mb: '40px' }}
-                >
-                  <TextField
-                    className={s.TextField}
-                    type="password"
-                    id="input-with-sx"
-                    label="Подтвердите пароль"
-                    variant="standard"
-                    fullWidth
-                    required
-                    name="confirmPassword"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.confirmPassword}
-                    InputProps={{
-                      startAdornment: (
-                        <LockIcon
-                          sx={{ color: 'action.active', mr: 1, my: 0.5 }}
-                        />
-                      ),
+                  >
+                    <TextField
+                      className={s.TextField}
+                      type="password"
+                      id="input-with-sx"
+                      label="Подтвердите пароль"
+                      variant="standard"
+                      fullWidth
+                      required
+                      name="confirmPassword"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.confirmPassword}
+                      InputProps={{
+                        startAdornment: (
+                          <LockIcon
+                            sx={{ color: 'action.active', mr: 1, my: 0.5 }}
+                          />
+                        ),
+                      }}
+                      placeholder="Подтвердите пароль"
+                    />
+                  </Box>
+                  {touched.name && errors.name && (
+                    <p className={s.error}>{errors.name}</p>
+                  )}
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'flex-end',
+                      mb: '40px',
+                      width: '100%',
                     }}
-                    placeholder="Подтвердите пароль"
-                  />
-                </Box>
-                {touched.name && errors.name && (
-                  <p className={s.error}>{errors.name}</p>
-                )}
-                <Box
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'flex-end',
-                    mb: '40px',
-                  }}
-                >
-                  <TextField
-                    className={s.TextField}
-                    id="input-with-sx"
-                    label="Ваше имя"
-                    type="text"
-                    variant="standard"
-                    fullWidth
-                    required
-                    name="name"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
-                    InputProps={{
-                      startAdornment: (
-                        <LocalPostOfficeIcon
-                          sx={{ color: 'action.active', mr: 1, my: 0.5 }}
-                        />
-                      ),
-                    }}
-                    placeholder="Ваше имя"
-                  />
-                </Box>
+                  >
+                    <TextField
+                      className={s.TextField}
+                      id="input-with-sx"
+                      label="Ваше имя"
+                      type="text"
+                      variant="standard"
+                      fullWidth
+                      required
+                      name="name"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.name}
+                      InputProps={{
+                        startAdornment: (
+                          <LocalPostOfficeIcon
+                            sx={{ color: 'action.active', mr: 1, my: 0.5 }}
+                          />
+                        ),
+                      }}
+                      placeholder="Ваше имя"
+                    />
+                  </Box>
 
-                <NavLink
-                  disabled={!isValid && !dirty}
-                  onClick={''}
-                  type={`submit`}
-                  to="/login"
-                  className={setActiveClass}
-                  data-name={'login'}
-                  id="login"
-                  style={({ isActive }) => ({
-                    marginBottom: isActive ? '20px' : '0px',
-                  })}
-                >
-                  Вход
-                </NavLink>
-                <NavLink
-                  disabled={!isValid && !dirty}
-                  onClick={handleSubmit}
-                  type={`submit`}
-                  to="/register"
-                  className={setActiveClass}
-                  data-name={'register'}
-                  id="register"
-                  style={({ isActive }) => ({
-                    marginTop: isActive ? '20px' : '0px',
-                    marginBottom: '0px',
-                  })}
-                >
-                  Регистрация
-                </NavLink>
-              </>
-            )}
+                  <NavLink
+                    disabled={!isValid && !dirty}
+                    onClick={''}
+                    type={`submit`}
+                    to="/login"
+                    className={setActiveClass}
+                    data-name={'login'}
+                    id="login"
+                    style={({ isActive }) => ({
+                      marginBottom: isActive ? '20px' : '0px',
+                    })}
+                  >
+                    Вход
+                  </NavLink>
+                  <NavLink
+                    disabled={!isValid && !dirty}
+                    onClick={handleSubmit}
+                    type={`submit`}
+                    to="/register"
+                    className={setActiveClass}
+                    data-name={'register'}
+                    id="register"
+                    style={({ isActive }) => ({
+                      marginTop: isActive ? '20px' : '0px',
+                      marginBottom: '0px',
+                    })}
+                  >
+                    Регистрация
+                  </NavLink>
+                </>
+              )}
 
-            {/*<label style={styles.label}>
+              {/*<label style={styles.label}>
           Name
           <input type="text" name="name" value={name} onChange={handleChange} />
         </label>
@@ -277,9 +295,10 @@ export default function RegisterView() {
         <button className="button" type="submit">
           Sign up
         </button>*/}
-          </Formik>
+            </Formik>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
