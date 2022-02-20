827dc6ccbf0994808f33182bf81c29ab18372e64
serg-grish <balchyg@icloud.com>
2022-02-20 16:59:27 +0200

    diagram fix

--- src/components/Diagramtab/styles.js
+++ src/components/Diagramtab/styles.js
@@ -1,12 +1,44 @@
 import styled from 'styled-components';
 
+import { css } from 'styled-components';
 
-export const StatsSheet = styled.div`
-    width: 280px;
-    margin-left: auto;
-    margin-right: auto;
+const breakpoints = {
+  s: '320px',
+  m: '768px',
+  l: '1280px',
+};
 
-    display: flex;
-    padding-top: 30px;
-  }
+export const mediaQuery = {
+  s: `(min-width: ${breakpoints.s})`,
+  m: `(min-width: ${breakpoints.m})`,
+  l: `(min-width: ${breakpoints.l})`,
+};
+
+
+export const breakpoint = Object.keys(breakpoints).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (min-width: ${breakpoints[label]}) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
+
+export const StatisticBox = styled.div`
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  ${breakpoint.m`
+    flex-direction: row;
+    align-items: normal;
+  `}
+`;
+
+export const StatisticsWrapper = styled.div`
+  width: 280px;
+  ${breakpoint.m`
+    width: 350px;
+  `}
 `;
