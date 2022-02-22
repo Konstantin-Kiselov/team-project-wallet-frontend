0063f32ca0e6b35b2b2d426237cd1b607286df83
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-22 21:00:02 +0100

    Update Registration.jsx

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -1,5 +1,17 @@
+import Container from '../Container/Container';
+
+import RegLeftSide from './RegLeftSide/RegLeftSide';
+import RegRightSide from './RegRightSide/RegRightSide';
+
 import s from './Registration.module.css';
 
-export default function Registration({ children }) {
-  return <section className={s.sectionRegistration}>{children}</section>;
+export default function Registration() {
+  return (
+    <section className={s.sectionRegistration}>
+      <Container>
+        <RegLeftSide />
+        <RegRightSide />
+      </Container>
+    </section>
+  );
 }
