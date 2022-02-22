4a5265fc9b5aea779ce6d73ab47dcd83bb0705e0
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-22 20:58:33 +0100

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,25 +3,25 @@ import s from './Container.module.css';
 import { motion } from 'framer-motion';
 
 function Container({ children }) {
-  const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: { opacity: 1, transition: { duration: 0.5 } },
-    exit: {
-      x: '-100vw',
-      transition: { ease: 'easeInOut' },
-    },
-  };
+  //const containerVariants = {
+  //  hidden: { opacity: 0 },
+  //  visible: { opacity: 1, transition: { duration: 0.5 } },
+  //  exit: {
+  //    x: '-100vw',
+  //    transition: { ease: 'easeInOut' },
+  //  },
+  //};
 
   return (
-    <motion.div
+    <div
       className={s.container}
-      variants={containerVariants}
-      initial="hidden"
-      animate="visible"
-      exit="exit"
+      //variants={containerVariants}
+      //initial="hidden"
+      //animate="visible"
+      //exit="exit"
     >
       {children}
-    </motion.div>
+    </div>
   );
 }
 
