eec3314609de6510dfae7f9232afd459744c08c5
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2022-02-22 11:02:00 +0100

    fixes problem with key in Currency adds new logics to Balance component to refresh removes unnecessary boarder in homeView

--- src/components/TotalBalance/TotalBalance.jsx
+++ src/components/TotalBalance/TotalBalance.jsx
@@ -1,15 +1,26 @@
 import s from './TotalBalance.module.css';
-import { useSelector } from 'react-redux';
+import { useMemo, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { authSelectors } from '../../redux/auth';
+import { fetchTransactions } from '../../redux/transactions/transactions-operations';
+import { getAllTransactions } from '../../redux/transactions/transactions-selector';
 
 export default function TotalBalance() {
-  const balance = useSelector(authSelectors.getUserBalance);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTransactions());
+  }, [dispatch]);
+
+  const transactions = useSelector(getAllTransactions);
+  const data = useMemo(() => transactions, [transactions]);
+  const balance = data.length === 0 ? 0 : data[data.length - 1].total;
 
   return (
     <div className={s.balanceLabel}>
       <p className={s.balanceText}>ВАШ БАЛАНС:</p>
       <p className={s.balanceValue}>
-        {balance && <span>$</span>} {balance}
+        <span>$</span> {balance}
       </p>
     </div>
   );
