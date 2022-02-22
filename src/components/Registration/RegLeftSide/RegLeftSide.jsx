97ec61f7583be34e6809cbfa05fc1afea566b0d6
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-22 21:00:12 +0100

    Create RegLeftSide.jsx

---
+++ src/components/Registration/RegLeftSide/RegLeftSide.jsx
@@ -0,0 +1,11 @@
+import RegFrameImac from '../RegFrameImac/RegFrameImac';
+
+import s from './RegLeftSide.module.css';
+
+export default function RegLeftSide() {
+  return (
+    <div className={s.RegLeftSide}>
+      <RegFrameImac />
+    </div>
+  );
+}
