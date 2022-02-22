86edca16259a64954be2aaf83dc832e535d970fb
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2022-02-22 16:58:51 +0100

    disables draft code

--- src/components/TotalBalance/TotalBalance.jsx
+++ src/components/TotalBalance/TotalBalance.jsx
@@ -1,27 +1,39 @@
 import s from './TotalBalance.module.css';
-import { useMemo, useEffect, useState } from 'react';
+import { useMemo, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { authSelectors } from '../../redux/auth';
 import { fetchTransactions } from '../../redux/transactions/transactions-operations';
 import { getAllTransactions } from '../../redux/transactions/transactions-selector';
 
 export default function TotalBalance() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchTransactions());
-  }, [dispatch]);
-
-  const transactions = useSelector(getAllTransactions);
-  const data = useMemo(() => transactions, [transactions]);
-  const balance = data.length === 0 ? 0 : data[data.length - 1].total;
+  const balance = useSelector(authSelectors.getUserBalance);
 
   return (
     <div className={s.balanceLabel}>
       <p className={s.balanceText}>ВАШ БАЛАНС:</p>
       <p className={s.balanceValue}>
-        <span>$</span> {balance}
+        {balance && <span>$</span>} {balance}
       </p>
     </div>
   );
 }
+
+// ============ balance with useEfect =================
+// ============ DRAFT TO CHECK ========================
+//   const dispatch = useDispatch();
+//   useEffect(() => {
+//     dispatch(fetchTransactions());
+//   }, [dispatch]);
+
+//   const transactions = useSelector(getAllTransactions);//
+//   const balance = transactions.length === 0 ? 0 : transactions[transactions.length - 1].total;
+
+//   return (
+//     <div className={s.balanceLabel}>
+//       <p className={s.balanceText}>ВАШ БАЛАНС:</p>
+//       <p className={s.balanceValue}>
+//         <span>$</span> {balance}
+//       </p>
+//     </div>
+//   );
+// }
