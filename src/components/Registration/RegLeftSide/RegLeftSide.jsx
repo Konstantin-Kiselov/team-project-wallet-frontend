85fdedd9c8f1ee69222eafa271c0e1455e104d14
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2022-02-22 22:36:31 +0100

    Update RegLeftSide.jsx

--- src/components/Registration/RegLeftSide/RegLeftSide.jsx
+++ src/components/Registration/RegLeftSide/RegLeftSide.jsx
@@ -1,11 +1,25 @@
 import RegFrameImac from '../RegFrameImac/RegFrameImac';
 
+import violetEllipse from '../../../img/log&reg/ellipse_violet.svg';
+
+import { motion } from 'framer-motion';
+
 import s from './RegLeftSide.module.css';
 
 export default function RegLeftSide() {
   return (
     <div className={s.RegLeftSide}>
       <RegFrameImac />
+
+      <div className={s.violetEllipse}>
+        <motion.img
+          src={violetEllipse}
+          alt="Violet Ellipse"
+          initial={{ x: '-100vw' }}
+          animate={{ x: 0 }}
+          transition={{ delay: 0.2, duration: 1.5 }}
+        />
+      </div>
     </div>
   );
 }
